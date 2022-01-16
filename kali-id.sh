baa055e182a5be3779e2749bcc8a6bfa6a2a3217
Has Root? <97832435+Pejuang-R00T@users.noreply.github.com>
2022-01-16 18:50:25 +0700

    Add files via upload

---
+++ kali-id.sh
@@ -0,0 +1,195 @@
+####################
+# CODE BY MR.T1T4N #
+####################
+
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+b='\033[1;34m'
+p='\033[1;35m'
+reset='\033[0m'
+clear
+echo ""
+echo $red"██╗  ██╗ █████╗ ██╗     ██╗      ██╗██████╗ "
+sleep 0.5
+echo $red"██║ ██╔╝██╔══██╗██║     ██║      ██║██╔══██╗"
+sleep 0.5
+echo $red"█████╔╝ ███████║██║     ██║█████╗██║██║  ██║"
+sleep 0.5
+echo $cyan"██╔═██╗ ██╔══██║██║     ██║╚════╝██║██║  ██║"
+sleep 0.5
+echo $cyan"██║  ██╗██║  ██║███████╗██║      ██║██████╔╝"
+sleep 0.5
+echo $cyan"╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝      ╚═╝╚═════╝ "
+
+
+echo $yellow"        ++++++++++++++++      "
+sleep 0.4
+echo $red   "       CODE BY MR.T1T4N      "
+sleep 0.4
+echo $yellow"        ++++++++++++++++      "
+sleep 0.4
+echo
+sleep 0.4
+echo
+sleep 0.4
+echo $red "     ×××××××××××××××××     "
+sleep 0.4
+echo $cyan"      Made In Indonesia     "
+sleep 0.4
+echo $red "     ×××××××××××××××××     "
+sleep 0.4
+echo
+sleep 0.4
+echo $b "     { ============= }    "
+sleep 0.4
+echo $cyan"       YT : T1T4N TZY   "
+sleep 0.4
+echo $b "     { ============= }    "
+echo
+echo
+sleep 20.5
+clear
+echo $b "    Waiting Has Loading.."
+echo $b "    [ $red==>             $b]"
+sleep 0.4
+clear
+echo $b "    Waiting Has Loading..."
+echo $b "    [ $red===>            $b]"
+sleep 0.4
+clear
+echo $b "    Waiting Has Loading.."
+echo $b "    [ $red====>           $b]"
+sleep 0.4
+clear
+echo $b "    Waiting Has Loading..."
+echo $b "    [ $red=====>          $b]"
+sleep 0.4
+clear
+echo $b "    Waiting Has Loading.."
+echo $b "    [ $red======>         $b]"
+sleep 0.4
+clear
+echo $b "    Waiting Has Loading..."
+echo $b "    [ $red=======>        $b]"
+sleep 0.4
+clear
+echo $b "    Waiting Has Loading.."
+echo $b "    [ $red========>       $b]"
+sleep 0.4
+clear
+echo $b "    Waiting Has Loading..."
+echo $b "    [ $red=========>      $b]"
+sleep 0.4
+clear
+echo $b "    Waiting Has Loading.."
+echo $b "    [ $red==========>     $b]"
+sleep 0.4
+clear
+echo $b "    Waiting Has Loading..."
+echo $b "    [ $red===========>    $b]"
+sleep 0.4
+clear
+echo $b "    Waiting Has Loading.."
+echo $b "    [ $red============>   $b]"
+sleep 0.4
+clear
+echo $b "    Waiting Has Loading..."
+echo $b "    [ $red=============>  $b]"
+sleep 0.4
+clear
+echo $b "    Waiting Has Loading.."
+echo $b "    [ $red==============> $b]"
+sleep 10
+clear
+echo
+echo $red "██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗"
+sleep 0.4
+echo $red "██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝"
+sleep 0.4
+echo $red "██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  "
+sleep 0.4
+echo $cyan "██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  "
+sleep 0.4
+echo $cyan "╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗"
+sleep 0.5
+echo $cyan " ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝"
+echo
+echo
+echo
+echo $yellow"            ++++++++++++++++      "
+sleep 0.4
+echo $red   "           CODE BY MR.T1T4N      "
+sleep 0.4
+echo $yellow"            ++++++++++++++++      "
+sleep 0.4
+echo $yellow"       { MY TEAM IS MY SUPPORT }"
+sleep 0.4
+echo
+sleep 0.4
+echo
+sleep 0.4
+echo $red "          ×××××××××××××××××     "
+sleep 0.4
+echo $cyan"           Made In Indonesia     "
+sleep 0.4
+echo $red "          ×××××××××××××××××     "
+sleep 0.4
+echo
+sleep 0.4
+echo $b "          { ============= }    "
+sleep 0.4
+echo $cyan"            YT : T1T4N TZY   "
+sleep 0.4
+echo $b "          { ============= }    "
+echo
+echo
+sleep 0.4
+echo $b "    { THANKS TO : MUGIWARA BLACKHAT || LEGEND CYBER TEAM } "
+sleep 0.4
+echo $b "  { AND SPECIAL TO : MR.N4N0 || MR.DENVAS || MR.RSDN 0198 } "
+echo
+sleep 10.5
+echo
+echo $red "××××××=== $cyanPleas Waitting $red===××××××"
+sleep 0.4
+cd $HOME
+
+apt-get update -y
+pkg install wget -y
+pkg install python -y
+pkg install python2 -y
+pkg install openssh -y
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+
+ls
+
+cd kali-idlnx
+
+ls
+
+cp $HOME
+
+ls
+
+rm rf kali-idlnx
+
+ls
+
+
+echo "$red-----> $cyan { Coppy Bellow Command } $red<-----"
+echo
+cat lol.txt
+echo
+echo "$red------> $cyan { Installion Has Completed } $red<-----"
+echo
+sleep 40
+echo "$red------> $cyan { Kali Linux Has Started } $red<-----"
+echo
+sleep 4.5
+echo
+clear
+rm lol.txt
+echo
+./kali-start.sh
